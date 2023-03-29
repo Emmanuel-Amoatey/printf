415b527cd30f911b9297f96b9388045d0fee1218
OffeiNewton <solomonwright9@gmail.com>
2023-03-28 17:28:47 -0700

    updated C-printf Collaborative task

--- get_precision.c
+++ get_precision.c
@@ -3,36 +3,38 @@
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * @i: Pointer to current index in the format string
+ * @list: va_list of arguments to be printed
  *
- * Return: Precision.
+ * Return: Precision
  */
 int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
 	int precision = -1;
 
+	/* If the next character is not '.', return -1 */
 	if (format[curr_i] != '.')
 		return (precision);
 
 	precision = 0;
+	curr_i++;
 
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	/* If the next character is '*', get precision from arguments */
+	if (format[curr_i] == '*')
 	{
-		if (is_digit(format[curr_i]))
+		curr_i++;
+		precision = va_arg(list, int);
+	}
+	else
+	{
+		/* Get precision from characters in format string */
+		while (is_digit(format[curr_i]))
 		{
 			precision *= 10;
 			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
 			curr_i++;
-			precision = va_arg(list, int);
-			break;
 		}
-		else
-			break;
 	}
 
 	*i = curr_i - 1;
